c08a1a2e1e6fed57c3fc32446f8380116129ac13
Aniket Kumar <62889360+aniketak47@users.noreply.github.com>
2025-01-27 15:36:52 +0530

    Time: 14 ms (13.16%), Space: 45.7 MB (82.32%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,29 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int finder = 0;
-        for(int val: nums) {
-            finder = finder ^ val;
+        int ans = nums[0];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int val:nums) {
+            if(map.containsKey(val)) {
+                map.put(val, map.get(val)+1);
+            } else {
+                map.put(val, 1);
+            }
         }
 
-        return finder;
+        for(Map.Entry<Integer, Integer> set : map.entrySet()) {
+            if(set.getValue() == 1) {
+                ans = set.getKey();
+            }
+        }
+        return ans;
     }
+    
+    // public int singleNumber(int[] nums) {
+    //     int finder = 0;
+    //     for(int val: nums) {
+    //         finder = finder ^ val;
+    //     }
+
+    //     return finder;
+    // }
 }
